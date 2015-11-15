beb25bd48ab3b4b2bf37028c50d02622ad6c86b4
Antoshka94 <kravchenko1anton@gmail.com>
2015-11-15 16:25:48 +0400

    GetSample() в колебат КФ
    добавил половинку метода

--- RandomProcess/RandomProcess/RandomProcess/OscillatoryCF.cs
+++ RandomProcess/RandomProcess/RandomProcess/OscillatoryCF.cs
@@ -19,7 +19,19 @@ namespace RandomProcess
 
 		public double GetSample()
 		{
-			throw new System.NotImplementedException();
+            double dt = 0.0785 / A;
+            double gamma = A * dt;
+            double gamma0 = W * dt;
+            double p = Math.Exp(-A);
+            double alfa0 = p * (Math.Pow(p, 2) - 1) * Math.Cos(gamma0);
+            double alfa1 = 1 - Math.Pow(p, 4);
+           // double a0 = Math.Sqrt(); как тут сделать +- ?
+            double a1 = alfa0 / alfa1;
+            double b1 = 2 * p * Math.Cos(gamma0);
+            double b2 = -Math.Pow(p,2);
+
+            double yn=0;
+            return yn;
 		}
 
 		public OscillatoryCF(double A, double W)
